df8a6880335f74e0df8e96c6e11baa4d20fb36a8
Leina85 <leinajen@icloud.com>
2025-07-02 14:22:10 +0100

    function calls enclosed in overaching main function

--- main_pipeline.py
+++ main_pipeline.py
@@ -1,32 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-from PIL import Image
-Image.MAX_IMAGE_PIXELS = None
-
 import sys
-sys.path.append(r"C:\Users\Leina School\Desktop\Work Exp BDI\BDI-Nuclei_Segmentation\pipeline")
+import typer
+from PIL import Image
 
+#sys.path.append(r"C:\Users\Leina School\Desktop\Work Exp BDI\BDI-Nuclei_Segmentation\pipeline")
 from img_manipulation import format_img, tile_img, seperate_tile_types
 from find_means import find_means
 from run_cellSAM import cellSAM
 
-image = np.array(Image.open(r"C:\Users\Leina School\Desktop\Work Exp BDI\data\GTEX-113JC-2226.jpg")) 
+Image.MAX_IMAGE_PIXELS = None
 
-w_target, h_target = 55750, 40500
-tile_size = 250
+#image = np.array(Image.open(r"C:\Users\Leina School\Desktop\Work Exp BDI\data\GTEX-113JC-2226.jpg"))
+#w_target, h_target = 55750, 40500
+#tile_size = 250
 
-cropped_image = format_img(image, w_target, h_target)
+def main(image:str, w_target:int=55750, h_target:int=40500, tile_size:int=250):
+    cropped_image = format_img(image, w_target, h_target)
+    tiles = tile_img(cropped_image, tile_size)
+    means = find_means(tiles)
+    tissue_tiles, background_tiles = seperate_tile_types(means)
+    mask = cellSAM(tiles[tissue_tiles[967]])
 
-tiles = tile_img(cropped_image, tile_size)
+    plt.imshow(mask)
+    plt.show()
+    plt.imshow(tiles[tissue_tiles[967]])
+    plt.show()
+    
+    #tissue_tiles, background_tiles = seperate_tile_types(find_means(tile_img(cropped_image, tile_size)))
+    #cellSAM(tile_img(format_img(image, w_target, h_target), tile_size)[tissue_tiles[967]])
 
-means = find_means(tiles)
-
-tissue_tiles, background_tiles = seperate_tile_types(means)
-
-mask = cellSAM(tiles[tissue_tiles[967]])
-
-plt.imshow(mask)
-plt.show()
-plt.imshow(tiles[tissue_tiles[967]])
-plt.show()
+if __name__ == "__main__":
+    typer.run(main)
