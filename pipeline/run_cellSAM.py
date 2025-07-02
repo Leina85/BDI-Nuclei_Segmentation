ddbe193c731aad5f0ca02cd1ae4fb9647cd61a51
Leina85 <leinajen@icloud.com>
2025-07-02 12:55:52 +0100

    Added comments

--- pipeline/run_cellSAM.py
+++ pipeline/run_cellSAM.py
@@ -2,9 +2,15 @@ from cellSAM import cellsam_pipeline
 import matplotlib.pyplot as plt
 
 def cellSAM(tile):
+    # Parameters: tile (NumPy array representing a specific tile to be process for nuclei identification)
+    # Returns: mask (NumPy array showing the models identification of the nuclei in the tile)
+    
+    
     mask = cellsam_pipeline(tile, use_wsi=True, low_contrast_enhancement=False, gauge_cell_size=False)
     
     return mask 
+
+
     #masks = []
 
     #for tile in tqdm(tissue_tile):
