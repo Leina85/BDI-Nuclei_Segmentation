684c5131db055326cef652a47efbd804103893bb
Leina85 <leinajen@icloud.com>
2025-07-02 12:45:41 +0100

    Added thorough comments and corrected inconsistant variable names

--- pipeline/img_manipulation.py
+++ pipeline/img_manipulation.py
@@ -2,36 +2,53 @@ from PIL import Image
 import numpy as np
 
 def format_img(base_image, w_target, h_target):
+    # Parameters: base_image (NumPy array representing the entire original histology image)
+    #             w_target (integer)
+    #             h_target (integer)
+    # Returns: cropped_image (NumPy array represending the cropped version of the original image for cleaner tiling)
+
+    # Crop the image to the inputted width and height
     cropped_image = base_image[:w_target, :h_target]
-    cropped_pil_image = Image.fromarray(cropped_image)
-    image_arr = np.array(cropped_pil_image)
     
-    return(image_arr)
+    return cropped_image 
+
 
 def tile_img(np_image, tile_size):
+    # Parameters: np_image (NumPy array of (cropped) image)
+    #             tile_size (integer)
+    # Returns: tiles (list of NumPy arrays representing each image tile)
+    
+    # Determines number of tile along the height and width of the image
     h_tile_num = np_image.shape[0]//tile_size
     w_tile_num = np_image.shape[1]//tile_size
 
+    # Initialize list to store the tiles
     tiles = []
 
+    # Loops over the image grid to extract tiles
     for y in range(h_tile_num):
         for x in range(w_tile_num):
-            #start:stop(not inclusive)
+            # Extract tile using slicing: [start_y:end_y, start_x:end_x, all_channels] does not seperate into colour channels (stays RGB)
             tile = np_image[y*tile_size:(y+1)*tile_size, x*tile_size:(x+1)*tile_size, :]
             tiles.append(tile)
             
-    return(tiles)
+    return tiles
 
-def seperate_background(means):
-    background_tile = []
-    tissue_tile = []
+
+def seperate_tile_types(means):
+    # Parameters: means (list of NumPy arrays representing RGB image sections)
+    # Returns: tissue_tile (list of floats representing mean greyscale values)
+    
+    # Initialize lists to store the background tiles and tiles containing tissue
+    background_tiles = []
+    tissue_tiles = []
 
     #for every tile, check avg, if surpasses threshhold then store index in array
     for tile in range(len(means)):
        print(means[tile])
        if means[tile] < 225:
-            tissue_tile.append(tile)
+            tissue_tiles.append(tile)
        else:
-          background_tile.append(tile)
+          background_tiles.append(tile)
           
-    return(tissue_tile, background_tile)
+    return tissue_tiles, background_tiles
