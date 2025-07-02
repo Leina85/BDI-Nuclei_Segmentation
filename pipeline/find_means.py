1b760e3e656a130a5d7d1a409c40b87215c30ed7
Leina85 <leinajen@icloud.com>
2025-07-02 12:46:49 +0100

    Added comments

--- pipeline/find_means.py
+++ pipeline/find_means.py
@@ -3,15 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def find_means(tiles):
+    # Parameters: tiles (list of NumPy arrays representing RGB image sections)
+    # Returns: means (list of floats representing mean greyscale values)
+    
+    # Initialize list to store mean values
     means = []
 
-    #find averages of each tile pixel values in greyscale
+    # Process each tile
     for tile in tiles:
+        # Convert NumPy array to PIL Image to change to greyscale
         img_section = Image.fromarray(tile)
         grey_img = img_section.convert("L")
+        
+        # Convert greyscale image back to NumPy array to compute and store mean
         grey_tile = np.array(grey_img)
-        means.append(grey_tile.mean()) 
-
+        means.append(grey_tile.mean())
+        
+    # Plot histogram of mean greyscale values to visualise colour distribution
     plt.hist(means, bins = 250)
+    plt.show()
     
-    return(means)
+    return means 
