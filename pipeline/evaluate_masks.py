e98ca0f31455dd457d1fd94974319cc92fed496c
Leina85 <leinajen@icloud.com>
2025-07-03 16:30:37 +0100

    added function to calculate dice coefficient to evaluate the cellSAM mask

--- pipeline/evaluate_masks.py
+++ pipeline/evaluate_masks.py
@@ -1,4 +1,17 @@
-from img_manipulation import format_tile
+import numpy as np
+from img_manipulation import format_tile, format_mask
 
 def segmentation_evaluate(cellSAM_mask, tile):
-    r = format_tile(tile)
+    bool_tile = format_tile(tile)
+    bool_mask = format_mask(cellSAM_mask)
+    dice_coefficient = dice_coefficient_calc(bool_tile, bool_mask)
+    
+    return dice_coefficient
+    
+def dice_coefficient_calc(set1, set2):
+    set1 = np.array(set1)
+    set2 = np.array(set2)
+    intersection = np.sum(np.logical_and(set1, set2))
+    dice_coefficient = 2. * intersection / (np.sum(set1) + np.sum(set2))
+    
+    return dice_coefficient
