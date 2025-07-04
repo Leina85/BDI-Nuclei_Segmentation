7d2fcdb3a02e14d89bad4683afb30dfc6562d9b0
Leina85 <leinajen@icloud.com>
2025-07-04 10:16:20 +0100

    boolean cellSAM mask and reformatted tiles are now saved to users computer

--- pipeline/evaluate_masks.py
+++ pipeline/evaluate_masks.py
@@ -1,10 +1,14 @@
 import numpy as np
 from img_manipulation import format_tile, format_mask
+from PIL import Image
 
 def segmentation_evaluate(cellSAM_mask, tile):
     bool_tile = format_tile(tile)
     bool_mask = format_mask(cellSAM_mask)
     dice_coefficient = dice_coefficient_calc(bool_tile, bool_mask)
+    
+    Image.fromarray(bool_tile).save("./BDI-Nuclei_Segmentation/results/bool_mask.jpg", format="JPEG")
+    Image.fromarray(bool_mask).save("./BDI-Nuclei_Segmentation/results/bool_tissue_tile.jpg", format="JPEG")
     
     return dice_coefficient
     
