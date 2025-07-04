e4801c2d91ff5c4bffcafe132142cc04ef9c27cd
Leina85 <leinajen@icloud.com>
2025-07-04 14:51:18 +0100

    iterate over tiles

--- main_pipeline.py
+++ main_pipeline.py
@@ -14,7 +14,7 @@ Image.MAX_IMAGE_PIXELS = None
 
 chosen_index = 100
 
-def main(image:str="BDI-Nuclei_Segmentation/data/GTEX-113JC-2226.jpg", w_target:int=55750, h_target:int=40500, tile_size:int=250):
+def main(image:str="BDI-Nuclei_Segmentation\data_jpeg\GTEX-113JC-2226.jpg", w_target:int=55750, h_target:int=40500, tile_size:int=250):
     print('main called')
     image = read_img(image)
     print("image downloaded")
@@ -22,19 +22,21 @@ def main(image:str="BDI-Nuclei_Segmentation/data/GTEX-113JC-2226.jpg", w_target:
     tiles = tile_img(cropped_image, tile_size)
     means = find_means(tiles)
     tissue_tiles, background_tiles = seperate_tile_types(means)
-    chosen_tile = tiles[tissue_tiles[chosen_index]]
-    mask = cellSAM(chosen_tile)
+    ##chosen_tile = tiles[tissue_tiles[chosen_index]]
+    for i in range(10):
+        chosen_tile = tiles[tissue_tiles[i*5]]
+        mask = cellSAM(chosen_tile)
     
-    # Make correct data type
-    mask_to_save = (mask / mask.max() * 255).astype(np.uint8)
-    tile_to_save = (chosen_tile / chosen_tile.max() * 255).astype(np.uint8)
+        # Make correct data type
+        mask_to_save = (mask / mask.max() * 255).astype(np.uint8)
+        tile_to_save = (chosen_tile / chosen_tile.max() * 255).astype(np.uint8)
     
-    # Save images
-    Image.fromarray(mask_to_save).save("./BDI-Nuclei_Segmentation/results/mask.jpg", format="JPEG")
-    Image.fromarray(tile_to_save).save("./BDI-Nuclei_Segmentation/results/tissue_tile.jpg", format="JPEG")
+        # Save images
+        Image.fromarray(mask_to_save).save(f"./BDI-Nuclei_Segmentation/results/mask{i}.jpg", format="JPEG")
+        Image.fromarray(tile_to_save).save(f"./BDI-Nuclei_Segmentation/results/tissue_tile{i}.jpg", format="JPEG")
     
-    dice_coefficient = segmentation_evaluate(mask, chosen_tile)
-    print(dice_coefficient)
+        dice_coefficient = segmentation_evaluate(mask, chosen_tile, i)
+        print('dice: ', dice_coefficient)
     
 if __name__ == "__main__":
     typer.run(main)
